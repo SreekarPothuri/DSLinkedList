1d00903e531aa0b34c1a23a4f752121d0442681d
Your Name <you@example.com>
2020-11-03 19:02:13 +0530

    Added: Test case for simple linkedlist to add at the top

---
+++ src/test/java/com/blz/dslinkedlist/TestLinkedList.java
@@ -0,0 +1,20 @@
+package com.blz.dslinkedlist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLinkedList {
+
+	@Test
+	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean res = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(res);
+	}
+}
